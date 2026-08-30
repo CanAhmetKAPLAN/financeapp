dd1d046b6035f3165f64b4fc523876485f351ddb
Can Ahmet KAPLAN <cakaplan06@gmail.com>
2026-08-30 18:47:05 +0300

    Controllers ve Dtos kuruldu.

--- api/Program.cs
+++ api/Program.cs
@@ -5,8 +5,9 @@ using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDBContext>(Options =>
 {
     Options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -17,10 +18,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
 
