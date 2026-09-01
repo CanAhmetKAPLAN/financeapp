c3ac43b2a00152603cc0d2faaab28f42f89ab5a9
Can Ahmet KAPLAN <cakaplan06@gmail.com>
2026-09-01 21:48:28 +0300

    AppUser-Stock Many-to-Many ilişki kuruldu.

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -17,18 +17,28 @@ namespace api.Data
         }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<Portfolio>(x => x.HasKey(p => new { p.AppUserId, p.StockId }));
+            builder.Entity<Portfolio>().HasOne(u => u.AppUser).WithMany(u => u.Portfolios).HasForeignKey(p => p.AppUserId);
+            builder.Entity<Portfolio>().HasOne(u => u.Stock).WithMany(u => u.Portfolios).HasForeignKey(p => p.StockId);
+
             List<IdentityRole> roles = new List<IdentityRole>
             {
                 new IdentityRole
                 {
+                    Id = "74a73cc5-e8dc-4cef-a953-0f4725c6ff03",
+                    ConcurrencyStamp = "dd13ce47-5416-49df-999e-8835b15fc791",
                     Name = "admin",
                     NormalizedName = "ADMIN"
                 },
                 new IdentityRole
                 {
+                    Id = "ec5b3bf3-1698-46e1-89d7-8cbce067c210",
+                    ConcurrencyStamp = "8811c972-7a67-467f-b304-c3b571dc3e12",
                     Name = "user",
                     NormalizedName = "USER"
                 },
