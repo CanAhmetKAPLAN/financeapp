e934127b2ad0722bf7a3e3e96255c4981e7e5cf2
Can Ahmet KAPLAN <cakaplan06@gmail.com>
2026-09-02 20:16:25 +0300

    Portfolio delete

--- api/Interfaces/IPortfolioRepository.cs
+++ api/Interfaces/IPortfolioRepository.cs
@@ -10,5 +10,6 @@ namespace api.Interfaces
     {
         Task<List<Stock>> GetUserPortfolio(AppUser appUser);
         Task<Portfolio> CreateAsync(Portfolio portfolio);
+        Task<Portfolio> DeletePortfolio(AppUser appUser, string symbol);
     }
 }
--- api/Repository/PortfolioRepository.cs
+++ api/Repository/PortfolioRepository.cs
@@ -36,5 +36,13 @@ namespace api.Repository
             await _context.SaveChangesAsync();
             return portfolio;
         }
+        public async Task<Portfolio> DeletePortfolio(AppUser appUser, string symbol)
+        {
+            var portfolioModel = await _context.Portfolios.FirstOrDefaultAsync(x => x.AppUserId == appUser.Id && x.Stock.Symbol.ToLower() == symbol.ToLower());
+            if (portfolioModel is null) return null;
+            _context.Portfolios.Remove(portfolioModel);
+            await _context.SaveChangesAsync();
+            return portfolioModel;
+        }
     }
 }
